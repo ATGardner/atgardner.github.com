5ff5cf35a3c136c10a03e07895f66b017d6d0676
Noam Gal <atgardner@gmail.com>
2017-02-03 22:45:35 +0200

    moved bundle and trails_data around fixed webpack config files added redirect script (running in the blogspot template)

--- src/trails.js
+++ src/trails.js
@@ -1,93 +1,96 @@
-import {createOlObject} from 'ol-wrapper'
+import { createOlObject } from 'ol-wrapper';
 
 const MAP_DEFAULTS = {
-    controls: [
-        {
-            olClass: 'control.Attribution'
-        },
-        {
-            olClass: 'control.ScaleLine',
-        },
-        {
-            olClass: 'control.Zoom',
-        },
-        {
-            olClass: 'control.ZoomSlider'
-        }
-    ],
-    loadTilesWhileAnimating: true,
-    loadTilesWhileInteracting: true
-}
+  controls: [
+    {
+      olClass: 'control.Attribution'
+    },
+    {
+      olClass: 'control.ScaleLine'
+    },
+    {
+      olClass: 'control.Zoom'
+    },
+    {
+      olClass: 'control.ZoomSlider'
+    }
+  ],
+  loadTilesWhileAnimating: true,
+  loadTilesWhileInteracting: true
+};
 const LAYER_DEFAULTS = {
-    preload: 7,
-
-    // for VectorLayer
-    updateWhileAnimating: true,
-    updateWhileInteracting: true
-}
+  preload: 7,
+  // for VectorLayer
+  updateWhileAnimating: true,
+  updateWhileInteracting: true
+};
 const VIEW_DEFAULTS = {
-    olClass: 'View',
-    maxZoom: 15,
-    zoom: 5
-}
+  olClass: 'View',
+  maxZoom: 15,
+  zoom: 5
+};
 const SOURCE_DEFAULTS = {
-    maxZoom: 15
-}
+  maxZoom: 15
+};
 
 async function getTrailData() {
-    trails = await fetch('/assets/trails/data.json')
-    return await trails.json();
+  trails = await fetch('/assets/trails_data/data.json');
+  return await trails.json();
 }
 
 async function getTrailConfig(trailName) {
-    const trails = await getTrailData()
-    const {trails: {[trailName]: trail}, layers} = trails
-    trail.layers = trail.layers.map(l => {
-        const layer = Object.assign({}, layers[l], LAYER_DEFAULTS)
-        layer.source = Object.assign({}, layer.source, SOURCE_DEFAULTS)
-        return layer
-    })
-    const pathClass = trail.path.split('.').pop().toUpperCase()
-    const format = `format.${pathClass}`
-    const pathLayer = {
-        olClass: 'layer.Vector',
-        type: 'overlay',
-        source: Object.assign({
-            olClass: 'source.Vector',
-            url: trail.path,
-            format: {
-                olClass: format
-            }
-        }, SOURCE_DEFAULTS)
-    }
-    if (pathClass === 'GPX') {
-        pathLayer.style = {
-            olClass: 'style.Style',
-            stroke: {
-                olClass: 'style.Stroke',
-                color: 'red',
-                width: 1
-            }
+  const trails = await getTrailData();
+  const { trails: { [trailName]: trail }, layers } = trails;
+  trail.layers = trail.layers.map(l => {
+    const layer = Object.assign({}, layers[l], LAYER_DEFAULTS);
+    layer.source = Object.assign({}, layer.source, SOURCE_DEFAULTS);
+    return layer;
+  });
+  const pathClass = trail.path.split('.').pop().toUpperCase();
+  const format = `format.${pathClass}`;
+  const pathLayer = {
+    olClass: 'layer.Vector',
+    type: 'overlay',
+    source: Object.assign(
+      {
+        olClass: 'source.Vector',
+        url: trail.path,
+        format: {
+          olClass: format
         }
-    }
+      },
+      SOURCE_DEFAULTS
+    )
+  };
+  if (pathClass === 'GPX') {
+    pathLayer.style = {
+      olClass: 'style.Style',
+      stroke: {
+        olClass: 'style.Stroke',
+        color: 'red',
+        width: 1
+      }
+    };
+  }
 
-    trail.layers.push(pathLayer)
-    trail.view = Object.assign({}, trail.view, VIEW_DEFAULTS)
-    return Object.assign({}, trail, MAP_DEFAULTS)
+  trail.layers.push(pathLayer);
+  trail.view = Object.assign({}, trail.view, VIEW_DEFAULTS);
+  return Object.assign({}, trail, MAP_DEFAULTS);
 }
 
 async function loadMapAsync(trailName, targetId) {
-    try {
-        const config = await getTrailConfig(trailName)
-        config.target = targetId
-        const map = createOlObject(config)
-        const view = map.getView()
-        view.fit(config.fit, map.getSize())
-    } catch (e) {
-        console.error('Failed loading map', e)
-    }
+  try {
+    const config = await getTrailConfig(trailName);
+    config.target = targetId;
+    const map = createOlObject(config);
+    const view = map.getView();
+    view.fit(config.fit, map.getSize());
+  } catch (e) {
+    console.error('Failed loading map', e);
+  }
 }
 
 export function loadMap(trailName, targetId) {
-    document.addEventListener('DOMContentLoaded', () => loadMapAsync(trailName, targetId))
+  document.addEventListener('DOMContentLoaded', () =>
+    loadMapAsync(trailName, targetId));
 }
